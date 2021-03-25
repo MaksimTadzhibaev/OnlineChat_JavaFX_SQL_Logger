27faf009cb3abee2f015db5533a415eff46c6198
Maksim <miromaxel@yandex.ru>
2021-03-26 00:02:07 +0300

    Решение

--- server/src/main/java/server/StartServer.java
+++ server/src/main/java/server/StartServer.java
@@ -1,9 +1,18 @@
 package server;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.*;
+import java.util.logging.LogManager;
 
 public class StartServer {
-    public static void main(String[] args) throws SQLException {
-        new Server();
+    public static void main(String[] args) {
+        try {
+            LogManager logManager = LogManager.getLogManager();
+            logManager.readConfiguration(new FileInputStream("logging.properties"));
+            new Server();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
